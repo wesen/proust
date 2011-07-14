c8bebf461e76d0de60e30c9ba7e54bac289d6c08
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-14 16:12:12 +0200

    Add tests for other types in parser

--- tests/testParser.php
+++ tests/testParser.php
@@ -9,6 +9,10 @@ require_once(dirname(__FILE__)."/../vendor/simpletest/autorun.php");
 require_once(dirname(__FILE__)."/../Mustache.php");
 
 class TestParser extends UnitTestCase {
+  function assertStartsWith($str, $prefix) {
+    $this->assertEqual(substr($str, 0, strlen($prefix)), $prefix);
+  }
+
   public function setUp() {
     $this->p = new Mustache\Parser();
   }
@@ -87,6 +91,184 @@ class TestParser extends UnitTestCase {
                                    array(":static", "text")
                                    ));
   }
+
+  public function testUnclosedTag() {
+    try {
+      $res = $this->p->compile("{{foo");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed tag");
+      $this->assertTrue(true);
+    }
+
+    try {
+      $res = $this->p->compile("{{foo{{bla}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed tag");
+      $this->assertTrue(true);
+    }
+
+    try {
+      $res = $this->p->compile("{{fo{o}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed tag");
+      $this->assertTrue(true);
+    }
+    
+  }
+
+  public function testIllegalContent() {
+    try {
+      $res = $this->p->compile("{{ #fo#o}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Illegal content in tag");
+      $this->assertTrue(true);
+    }
+  }
+
+  public function testUnclosedSection() {
+    try {
+      $res = $this->p->compile("{{#foo}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
+      $this->assertTrue(true);
+    }
+    
+    try {
+      $res = $this->p->compile("{{#foo}}bla");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
+      $this->assertTrue(true);
+    }
+
+    try {
+      $res = $this->p->compile("{{#foo}}{{#bla}}{{/bla}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
+      $this->assertTrue(true);
+    }
+    try {
+      $res = $this->p->compile("{{#foo}}{{#foo}}{{/foo}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
+      $this->assertTrue(true);
+    }
+
+    try {
+      $res = $this->p->compile("{{#foo}}{{#bla}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section bla");
+      $this->assertTrue(true);
+    }
+
+    try {
+      $res = $this->p->compile("{{#foo}}{{/bla}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
+      $this->assertTrue(true);
+    }
+
+    try {
+      $res = $this->p->compile("{{bla}}{{#foo}}{{/bla}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
+      $this->assertTrue(true);
+    }
+  }
+
+  public function testSimpleSection() {
+    $res = $this->p->compile("{{#foo}}{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":section", "foo", array(":multi"))));
+
+    $res = $this->p->compile("{{#foo}}text{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":section", "foo", array(":multi",
+                                                                               array(":static", "text")))));
+
+    $res = $this->p->compile("{{#foo}}{{foo}}{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":section", "foo", array(":multi",
+                                                                               array(":mustache", ":etag", "foo")))));
+
+    $res = $this->p->compile("{{^foo}}{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":inverted_section", "foo", array(":multi"))));
+
+    $res = $this->p->compile("{{^foo}}text{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":inverted_section", "foo", array(":multi",
+                                                                                        array(":static", "text")))));
+
+    $res = $this->p->compile("{{^foo}}{{foo}}{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":inverted_section", "foo", array(":multi",
+                                                                                        array(":mustache", ":etag", "foo")))));
+  }
+
+  public function testNestedSection() {
+    $res = $this->p->compile("{{#foo}}{{#bla}}{{/bla}}{{/foo}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":section", "foo", array(":multi",
+                                                                               array(":mustache", ":section", "bla", array(":multi"))))));
+  }
+
+  public function testUnopenedSection() {
+    try {
+      $res = $this->p->compile("{{/foo}}");
+      $this->assertFalse(true);
+    } catch (Mustache\SyntaxError $e) {
+      $this->assertStartsWith($e->getMessage(), "Closing unopened section foo");
+      $this->assertTrue(true);
+    }
+  }
+
+  public function testCtag() {
+    $res = $this->p->compile("{{=[[ ]]}}[[foo]]text{{text}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":etag", "foo"),
+                                   array(":static", "text{{text}}")));
+
+    /* create new mustache to reset ctag and otag. */
+    $this->p = new Mustache\Parser();
+    $res = $this->p->compile("{{=[[ ]]}}[[foo]]text[[={{ }}]]{{text}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":etag", "foo"),
+                                   array(":static", "text"),
+                                   array(":mustache", ":etag", "text")));
+  }
+
+  public function testPartial() {
+    $res = $this->p->compile("{{>partial}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":partial", "partial")));
+    $res = $this->p->compile("{{<partial}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":partial", "partial")));
+    $res = $this->p->compile("{{<partial/test}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":partial", "partial/test")));
+  }
+
+  public function testUtag() {
+    $res = $this->p->compile("{{{fresh}}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":utag", "fresh")));
+    $res = $this->p->compile("{{&unescaped_stuff}}");
+    $this->assertEqual($res, array(":multi",
+                                   array(":mustache", ":utag", "unescaped_stuff")));
+  }
+
 };
 
 ?>
