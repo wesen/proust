6b0c905e1a99c782e1286583762697e5e414a266
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-14 10:59:46 +0200

    Add comment to main file

--- Mustache.php
+++ Mustache.php
@@ -1,7 +1,16 @@
 <?php
 
+/*
+ * Mustache PHP Compiler
+ *
+ * This is a straight port of the ruby mustache compiler
+ *
+ * (c) July 2011 - Manuel Odendahl - wesen@ruinwesen.com
+ */
+
 define('MUSTACHE_PHP_VERSION_ID', '0.1');
 
-require 'lib/StringScanner.php';
+require_once('lib/StringScanner.php');
+require_once('lib/Mustache.php');
 
 ?>
