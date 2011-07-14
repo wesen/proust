bd45c67e7789f09a98a95c89f523fea29c477f98
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-14 12:31:50 +0200

    Added placeholder definition for template parser and generator

---
+++ lib/Parser.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Template PHP Compiler
+ *
+ * (c) July 2011 - Manuel Odendahl - wesen@ruinwesen.com
+ */
+
+namespace Mustache;
+
+class Parser {
+  public function __construct() {
+  }
+
+  public function compile($src) {
+    return array();
+  }
+}
+
+?>
--- lib/Template.php
+++ lib/Template.php
@@ -8,8 +8,39 @@
 
 namespace Mustache;
 
+require_once(dirname(__FILE__).'/Parser.php');
+require_once(dirname(__FILE__).'/Generator.php');
+
 class Template {
-  public function __construct($data) {
+  protected $data = "";
+  protected $compiled = null;
+  
+  public function __construct($_data) {
+    $this->data = $_data;
+  }
+
+  public function render($context) {
+    if ($this->compiled == null) {
+      $this->compiled = $this->compile($this->data);
+    }
+    
+    return $this->compiled($context);
+  }
+
+  public function compile($src = null) {
+    if ($src == null) {
+      $src = $this->data;
+    }
+    $generator = new Mustache\Generator();
+    return $generator->compile($this->getTokens($src));
+  }
+
+  public function getTokens($src = null) {
+    if ($src == null) {
+      $src = $this->data;
+    }
+    $parser = new Mustache\Parser();
+    return $parser->compile($src);
   }
 };
 
