f746d7bf9c6dcd4ee6fd2a11e9a9b951f08e8d30
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-14 12:11:23 +0200

    First implementation of context object (translated from ruby)

--- lib/Context.php
+++ lib/Context.php
@@ -8,23 +8,94 @@
 
 namespace Mustache;
 
+/**
+ * A ContextMissException is raised whenever a ta's target can not be
+ * found in the current context is Mustache->raiseOnContextMiss is set
+ * to true.
+ *
+ * By default, it is not raised.
+ **/
+class ContextMissException extends \Exception {
+};
+
 class Context implements \ArrayAccess {
-  public function __construct() {
+  protected $stack = null;
+  
+  public function __construct($_mustache) {
+    $this->stack = array($_mustache);
   }
 
-  function offsetExists( $offset ) {
-    return false;
-  }
-
-  function offsetGet ( $offset ) {
+  public function getMustacheInStack() {
+    foreach ($this->stack as $_s) {
+      if ($_s instanceof \Mustache) {
+        return $_s;
+      }
+    }
     return null;
   }
 
+  public function push($new) {
+    array_unshift($this->stack, $new);
+    return $this;
+  }
+
+  public function pop() {
+    array_shift($this->stack);
+    return $this;
+  }
+
+  public function fetch($name, $default = '__raise') {
+    foreach ($this->stack as $a) {
+      /* avoid recursion */
+      if ($a == $this) {
+        continue;
+      }
+
+      if (($a instanceof \ArrayAccess) || (is_array($a))) {
+        if (isset($a[$name])) {
+          return $a[$name];
+        } else if (isset($a[(string)$name])) {
+          return $a[(string)$name];
+        }
+      } elseif (method_exists($a, $name)) {
+        return $a->$name();
+      }
+
+      if (($default == '__raise') || $this->getMustacheInStack()->raiseOnContextMiss) {
+        throw new ContextMissException("Can't find $name in ".print_r($this->stack, true));
+      } else {
+        return $default;
+      }
+    }
+  }
+
+  /***************************************************************************
+   *
+   * Implement array access
+   *
+   ***************************************************************************/
+
+  function offsetExists( $offset ) {
+    try {
+      $this->fetch($offset);
+      return true;
+    } catch (ContextMissException $e) {
+      return false;
+    }
+  }
+
+  function offsetGet ( $offset ) {
+    return $this->fetch($offset);
+  }
+
+  /** Add a value to the context. **/
   function offsetSet ( $offset ,  $value ) {
+    $this->push(array($offset => $value));
   }
 
   function offsetUnset ( $offset ) {
+    throw new Exception("Can't remove from Context, use pop() instead");
   }  
-}
+};
 
 ?>
