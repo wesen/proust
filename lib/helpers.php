1af954afe8d9f24194402b05b536d5306d88e35f
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-15 11:39:33 +0200

    Add debug loggin function

--- lib/helpers.php
+++ lib/helpers.php
@@ -6,6 +6,11 @@
  * (c) July 2011 - Manuel Odendahl - wesen@ruinwesen.com
  */
 
+$DEBUG =
+       //       array('PARSER', 'NOTICE', 'COMPILER', 'EVALUATION')
+       array('PARSER')
+  ;
+  
 function get_class_name($object = null) {
   if (!is_object($object) && !is_string($object)) {
     return false;
@@ -13,6 +18,19 @@ function get_class_name($object = null) {
     
   $class = explode('\\', (is_string($object) ? $object : get_class($object)));
   return $class[count($class) - 1];
+}
+
+function objectSetOptions($obj, $options) {
+  foreach ($options as $k => $v) {
+    $obj->$k = $v;
+  }
+}
+
+function debug_log($str, $level = "NOTICE") {
+  global $DEBUG;
+  if (in_array($level, $DEBUG)) {
+    error_log($str);
+  }
 }
 
 ?>
