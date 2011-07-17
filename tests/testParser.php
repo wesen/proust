25113de154ff0556b6d4e384f67322814410776c
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-17 16:34:50 +0200

    Adapt tests to new interface

--- tests/testParser.php
+++ tests/testParser.php
@@ -18,63 +18,63 @@ class TestParser extends UnitTestCase {
   }
   
   public function testEmpty() {
-    $res = $this->p->compile("");
+    $res = $this->p->parse("");
     $this->assertEqual($res, array(":multi"));
   }
 
   public function testComment() {
-    $res = $this->p->compile("{{!foobar comment}}");
+    $res = $this->p->parse("{{!foobar comment}}");
     $this->assertEqual($res, array(":multi"));
-    $res = $this->p->compile("{{!foobar comment}}{{!foobar comment}}");
+    $res = $this->p->parse("{{!foobar comment}}{{!foobar comment}}");
     $this->assertEqual($res, array(":multi"));
   }
 
   public function testRawText() {
-    $res = $this->p->compile("foo");
+    $res = $this->p->parse("foo");
     $this->assertEqual($res, array(":multi", array(":static", "foo")));
 
-    $res = $this->p->compile("{{!comment}}foo");
+    $res = $this->p->parse("{{!comment}}foo");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "foo")));
-    $res = $this->p->compile("{{!comment}}foo{{!comment}}");
+    $res = $this->p->parse("{{!comment}}foo{{!comment}}");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "foo")));
   }
 
   public function testSimpleMustache() {
-    $res = $this->p->compile("{{foo}}");
+    $res = $this->p->parse("{{foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":etag", "foo")));
   }
 
   public function testTwoTags() {
-    $res = $this->p->compile("{{foo}}{{bla}}");
+    $res = $this->p->parse("{{foo}}{{bla}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":etag", "foo"),
                                    array(":mustache", ":etag", "bla")));
   }
 
   public function testTwoTagsComment() {
-    $res = $this->p->compile("{{foo}}{{!comment}}{{bla}}");
+    $res = $this->p->parse("{{foo}}{{!comment}}{{bla}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":etag", "foo"),
                                    array(":mustache", ":etag", "bla")));
   }
 
   public function testTagText() {
-    $res = $this->p->compile("text{{foo}}");
+    $res = $this->p->parse("text{{foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "text"),
                                    array(":mustache", ":etag", "foo")));
 
-    $res = $this->p->compile("text{{foo}}text");
+    $res = $this->p->parse("text{{foo}}text");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "text"),
                                    array(":mustache", ":etag", "foo"),
                                    array(":static", "text")
                                    ));
 
-    $res = $this->p->compile("text{{foo}}{{bla}}text");
+    $res = $this->p->parse("text{{foo}}{{bla}}text");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "text"),
                                    array(":mustache", ":etag", "foo"),
@@ -82,7 +82,7 @@ class TestParser extends UnitTestCase {
                                    array(":static", "text")
                                    ));
 
-    $res = $this->p->compile("text{{foo}}text2{{bla}}text");
+    $res = $this->p->parse("text{{foo}}text2{{bla}}text");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "text"),
                                    array(":mustache", ":etag", "foo"),
@@ -94,7 +94,7 @@ class TestParser extends UnitTestCase {
 
   public function testUnclosedTag() {
     try {
-      $res = $this->p->compile("{{foo");
+      $res = $this->p->parse("{{foo");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed tag");
@@ -102,7 +102,7 @@ class TestParser extends UnitTestCase {
     }
 
     try {
-      $res = $this->p->compile("{{foo{{bla}}");
+      $res = $this->p->parse("{{foo{{bla}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed tag");
@@ -110,7 +110,7 @@ class TestParser extends UnitTestCase {
     }
 
     try {
-      $res = $this->p->compile("{{fo{o}}");
+      $res = $this->p->parse("{{fo{o}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed tag");
@@ -121,7 +121,7 @@ class TestParser extends UnitTestCase {
 
   public function testIllegalContent() {
     try {
-      $res = $this->p->compile("{{ #fo#o}}");
+      $res = $this->p->parse("{{ #fo#o}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Illegal content in tag");
@@ -131,7 +131,7 @@ class TestParser extends UnitTestCase {
 
   public function testUnclosedSection() {
     try {
-      $res = $this->p->compile("{{#foo}}");
+      $res = $this->p->parse("{{#foo}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
@@ -139,7 +139,7 @@ class TestParser extends UnitTestCase {
     }
     
     try {
-      $res = $this->p->compile("{{#foo}}bla");
+      $res = $this->p->parse("{{#foo}}bla");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
@@ -147,14 +147,14 @@ class TestParser extends UnitTestCase {
     }
 
     try {
-      $res = $this->p->compile("{{#foo}}{{#bla}}{{/bla}}");
+      $res = $this->p->parse("{{#foo}}{{#bla}}{{/bla}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
       $this->assertTrue(true);
     }
     try {
-      $res = $this->p->compile("{{#foo}}{{#foo}}{{/foo}}");
+      $res = $this->p->parse("{{#foo}}{{#foo}}{{/foo}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
@@ -162,7 +162,7 @@ class TestParser extends UnitTestCase {
     }
 
     try {
-      $res = $this->p->compile("{{#foo}}{{#bla}}");
+      $res = $this->p->parse("{{#foo}}{{#bla}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section bla");
@@ -170,7 +170,7 @@ class TestParser extends UnitTestCase {
     }
 
     try {
-      $res = $this->p->compile("{{#foo}}{{/bla}}");
+      $res = $this->p->parse("{{#foo}}{{/bla}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
@@ -178,7 +178,7 @@ class TestParser extends UnitTestCase {
     }
 
     try {
-      $res = $this->p->compile("{{bla}}{{#foo}}{{/bla}}");
+      $res = $this->p->parse("{{bla}}{{#foo}}{{/bla}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Unclosed section foo");
@@ -187,39 +187,39 @@ class TestParser extends UnitTestCase {
   }
 
   public function testSimpleSection() {
-    $res = $this->p->compile("{{#foo}}{{/foo}}");
+    $res = $this->p->parse("{{#foo}}{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":section", "foo", array(":multi"), 8, 8)));
 
-    $res = $this->p->compile("{{#foo}}text{{/foo}}");
+    $res = $this->p->parse("{{#foo}}text{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":section", "foo", array(":multi",
                                                                                array(":static", "text")),
                                          8, 12)));
 
-    $res = $this->p->compile("{{#foo}}{{foo}}{{/foo}}");
+    $res = $this->p->parse("{{#foo}}{{foo}}{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":section", "foo", array(":multi",
                                                                                array(":mustache", ":etag", "foo")),
                                          8, 15)));
 
-    $res = $this->p->compile("{{^foo}}{{/foo}}");
+    $res = $this->p->parse("{{^foo}}{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":inverted_section", "foo", array(":multi"))));
 
-    $res = $this->p->compile("{{^foo}}text{{/foo}}");
+    $res = $this->p->parse("{{^foo}}text{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":inverted_section", "foo", array(":multi",
                                                                                         array(":static", "text")))));
 
-    $res = $this->p->compile("{{^foo}}{{foo}}{{/foo}}");
+    $res = $this->p->parse("{{^foo}}{{foo}}{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":inverted_section", "foo", array(":multi",
                                                                                         array(":mustache", ":etag", "foo")))));
   }
 
   public function testNestedSection() {
-    $res = $this->p->compile("{{#foo}}{{#bla}}{{/bla}}{{/foo}}");
+    $res = $this->p->parse("{{#foo}}{{#bla}}{{/bla}}{{/foo}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":section", "foo", array(":multi",
                                                                                array(":mustache", ":section", "bla", array(":multi"), 16, 16)),
@@ -228,7 +228,7 @@ class TestParser extends UnitTestCase {
 
   public function testUnopenedSection() {
     try {
-      $res = $this->p->compile("{{/foo}}");
+      $res = $this->p->parse("{{/foo}}");
       $this->assertFalse(true);
     } catch (Mustache\SyntaxError $e) {
       $this->assertStartsWith($e->getMessage(), "Closing unopened section foo");
@@ -237,7 +237,7 @@ class TestParser extends UnitTestCase {
   }
 
   public function testCtag() {
-    $res = $this->p->compile("{{=[[ ]]}}[[foo]]text{{text}}");
+    $res = $this->p->parse("{{=[[ ]]}}[[foo]]text{{text}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":tag_change", "[[", "]]"),
                                    array(":mustache", ":etag", "foo"),
@@ -245,7 +245,7 @@ class TestParser extends UnitTestCase {
 
     /* create new mustache to reset ctag and otag. */
     $this->p = new Mustache\Parser();
-    $res = $this->p->compile("{{=[[ ]]}}[[foo]]text[[={{ }}]]{{text}}");
+    $res = $this->p->parse("{{=[[ ]]}}[[foo]]text[[={{ }}]]{{text}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":tag_change", "[[", "]]"),
                                    array(":mustache", ":etag", "foo"),
@@ -255,32 +255,32 @@ class TestParser extends UnitTestCase {
   }
 
   public function testPartial() {
-    $res = $this->p->compile("{{>partial}}");
+    $res = $this->p->parse("{{>partial}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":partial", "partial", "")));
-    $res = $this->p->compile("{{<partial}}");
+    $res = $this->p->parse("{{<partial}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":partial", "partial", "")));
-    $res = $this->p->compile("{{<partial/test}}");
+    $res = $this->p->parse("{{<partial/test}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":partial", "partial/test", "")));
 
-    $res = $this->p->compile("  {{>partial}}");
+    $res = $this->p->parse("  {{>partial}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":partial", "partial", "  ")));
 
-    $res = $this->p->compile("foo  {{>partial}}");
+    $res = $this->p->parse("foo  {{>partial}}");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "foo  "),
                                    array(":mustache", ":partial", "partial", "")));
 
-    $res = $this->p->compile("foo  \n  {{>partial}}");
+    $res = $this->p->parse("foo  \n  {{>partial}}");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "foo  "),
                                    array(":newline"),
                                    array(":mustache", ":partial", "partial", "  ")));
 
-    $res = $this->p->compile("foo  \n\t\t  {{>partial}}");
+    $res = $this->p->parse("foo  \n\t\t  {{>partial}}");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "foo  "),
                                    array(":newline"),
@@ -288,16 +288,16 @@ class TestParser extends UnitTestCase {
   }
 
   public function testUtag() {
-    $res = $this->p->compile("{{{fresh}}}");
+    $res = $this->p->parse("{{{fresh}}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":utag", "fresh")));
-    $res = $this->p->compile("{{&unescaped_stuff}}");
+    $res = $this->p->parse("{{&unescaped_stuff}}");
     $this->assertEqual($res, array(":multi",
                                    array(":mustache", ":utag", "unescaped_stuff")));
   }
 
   public function testLong() {
-    $res = $this->p->compile('Hello {{name}}
+    $res = $this->p->parse('Hello {{name}}
 You have just won ${{value}}!
 {{#in_ca}}
 Well, ${{taxed_value}}, after taxes.
@@ -320,15 +320,15 @@ Well, ${{taxed_value}}, after taxes.
   }
 
   public function testWhitespace() {
-    $res = $this->p->compile(" ");
+    $res = $this->p->parse(" ");
     $this->assertEqual($res, array(":multi", array(":static", " ")));
 
-    $res = $this->p->compile("\r\n");
+    $res = $this->p->parse("\r\n");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "\r"),
                                    array(":newline")));
 
-    $res = $this->p->compile("\r\n\r\n");
+    $res = $this->p->parse("\r\n\r\n");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "\r"),
                                    array(":newline"),
@@ -337,14 +337,14 @@ Well, ${{taxed_value}}, after taxes.
   }
 
   public function testWhitespaceTag() {
-    $res = $this->p->compile("\r\n{{tag}}\r\n"); 
+    $res = $this->p->parse("\r\n{{tag}}\r\n"); 
     $this->assertEqual($res, array(":multi",
                                    array(":static", "\r"),
                                    array(":newline"),
                                    array(":mustache", ":etag", "tag"),
                                    array(":static", "\r"),
                                    array(":newline")));
-    $res = $this->p->compile("\r\n  {{tag}}\r\n"); 
+    $res = $this->p->parse("\r\n  {{tag}}\r\n"); 
     $this->assertEqual($res, array(":multi",
                                    array(":static", "\r"),
                                    array(":newline"),
@@ -355,24 +355,24 @@ Well, ${{taxed_value}}, after taxes.
   }
 
   public function testWhitespaceComment() {
-    $res = $this->p->compile("{{! comment }}   ");
+    $res = $this->p->parse("{{! comment }}   ");
     $this->assertEqual($res, array(":multi"));
 
-    $res = $this->p->compile("   {{! comment }}   ");
+    $res = $this->p->parse("   {{! comment }}   ");
     $this->assertEqual($res, array(":multi"));
-    $res = $this->p->compile("   {{! comment }}   \n");
+    $res = $this->p->parse("   {{! comment }}   \n");
     $this->assertEqual($res, array(":multi"));
 
-    $res = $this->p->compile("   {{! comment }}   \n  ");
+    $res = $this->p->parse("   {{! comment }}   \n  ");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "  ")));
 
-    $res = $this->p->compile("\r\n   {{! comment }}   \n");
+    $res = $this->p->parse("\r\n   {{! comment }}   \n");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "\r"),
                                    array(":newline")));
 
-    $res = $this->p->compile("\r\nfoo {{! comment}} bla\r\n blorg\r\n");
+    $res = $this->p->parse("\r\nfoo {{! comment}} bla\r\n blorg\r\n");
     $this->assertEqual($res, array(":multi",
                                    array(":static", "\r"),
                                    array(":newline"),
