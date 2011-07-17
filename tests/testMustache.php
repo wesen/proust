b861b4e6b6adfe1d30ec49eb9f0778f64f3aad0e
Manuel Odendahl <wesen@ruinwesen.com>
2011-07-17 13:28:36 +0200

    Adapt tests to new API

--- tests/testMustache.php
+++ tests/testMustache.php
@@ -15,132 +15,61 @@ function __filename($file) {
 
 require_once(dirname(__FILE__).'/classDefs.php');
 
-class FoobarBlorg extends Mustache {
-};
-
-function mustacheForFile($file) {
-  $m = new Mustache();
-  $m->setTemplateFile(dirname(__FILE__)."/files/$file");
-  return $m;
-}
-
 class TestMustache extends UnitTestCase {
-
+  function setUp() {
+    $this->m = new Mustache(array("templatePath" => dirname(__FILE__)."/files/"));
+  }
+  
   function testPartial() {
     $m = new Mustache();
-    $m->setTemplatePath(dirname(__FILE__)."/files/");
+    $m->templatePath = (dirname(__FILE__)."/files/");
     $res = $m->partial("testPartial");
     $this->assertEqual($res, "partial {{mustache}}\n");
 
     /* XXX test partial evaluation */
   }
 
-  function testClassify() {
-    $res = Mustache::classify("foobar");
-    $this->assertEqual($res, "Foobar");
-
-    $res = Mustache::classify("foobar_blorg");
-    $this->assertEqual($res, "FoobarBlorg");
-
-    $res = Mustache::classify("foobar_blorg_bla");
-    $this->assertEqual($res, "FoobarBlorgBla");
-  }
-
-  function testUnderscore() {
-    $res = Mustache::underscore("foobar");
-    $this->assertEqual($res, "foobar");
-
-    $res = Mustache::underscore("Foobar");
-    $this->assertEqual($res, "foobar");
-
-    $res = Mustache::underscore("FoobarBlorg");
-    $this->assertEqual($res, "foobar_blorg");
-
-    $res = Mustache::underscore("FoobarBlorgBla");
-    $this->assertEqual($res, "foobar_blorg_bla");
-  }
-
-  function testClassName() {
-    $res = new Mustache();
-    $this->assertEqual($res->getTemplateName(), "mustache");
-
-    $res = new FoobarBlorg();
-    $this->assertEqual($res->getTemplateName(), "foobar_blorg");
-
-    $res = new FooBar\Bla\FoobarBlorgBla();
-    $this->assertEqual($res->getTemplateName(), "foobar_blorg_bla");
-  }
-
   function testSetters() {
     $res = new Mustache();
-    $this->assertEqual($res->getTemplatePath(), ".");
-    $this->assertEqual($res->getTemplateName(), "mustache");
-    $this->assertEqual($res->getTemplateExtension(), "mustache");
-    $this->assertEqual($res->getTemplateFile(), "./mustache.mustache");
-
-    $res = new FoobarBlorg();
-    $this->assertEqual($res->getTemplatePath(), ".");
-    $this->assertEqual($res->getTemplateName(), "foobar_blorg");
-    $this->assertEqual($res->getTemplateExtension(), "mustache");
-    $this->assertEqual($res->getTemplateFile(), "./foobar_blorg.mustache");
-  }
-
-  function testTokens() {
-    $m = mustacheForFile("token1.mustache");
-    $tpl = $m->getTemplate();
-    $res = $tpl->getTokens();
-    $this->assertEqual($res, array(":multi",
-                                   array(":static", "foo"),
-                                   array(":newline")));
-
-    $m = mustacheForFile("token2.mustache");
-    $tpl = $m->getTemplate();
-    $res = $tpl->getTokens();
-    $this->assertEqual($res, array(":multi",
-                                   array(":mustache", ":etag", "foo"),
-                                   array(":newline")));
+    $this->assertEqual($res->templatePath, ".");
+    $this->assertEqual($res->templateExtension, "mustache");
   }
 
   function testRender() {
-    $m = mustacheForFile("token1.mustache");
-    $res = $m->render();
+    $m = $this->m;
+    
+    $res = $m->renderFile("token1");
     $this->assertEqual($res, "foo\n");
     /* try again for precompiled version. */
-    $res = $m->render();
+    $res = $m->renderFile("token1");
     $this->assertEqual($res, "foo\n");
 
-    $m = mustacheForFile("token2.mustache");
-    $res = $m->render();
+    $res = $m->renderFile("token2");
     $this->assertEqual($res, "\n");
     /* try again for precompiled version. */
-    $res = $m->render();
+    $res = $m->renderFile("token2");
     $this->assertEqual($res, "\n");
-
-    $res = $m->render(null, array("foo" => "foo"));
-    $this->assertEqual($res, "foo\n");
-    $res = $m->render(null, array("foo" => "bla"));
-    $this->assertEqual($res, "bla\n");
   }
 
   function testRenderSection() {
-    $m = mustacheForFile("section1.mustache");
-    $res = $m->render();
+    $m = $this->m;
+    $res = $m->renderFile("section1");
     $this->assertEqual($res, "\n");
-    $res = $m->render(null, array("foo" => array("bla" => "bla")));
+    $res = $m->renderFile("section1", array("foo" => array("bla" => "bla")));
     $this->assertEqual($res, "bla\n");
 
-    $res = $m->render(null, array("foo" => array(array("bla" => "1 "),
-                                                 array("bla" => "2 "),
-                                                 array("bla" => "3 "),
-                                                 array("bla" => "4 "),
-                                                 array("bla" => "5 ")
-                                                 )));
+    $res = $m->renderFile("section1", array("foo" => array(array("bla" => "1 "),
+                                                           array("bla" => "2 "),
+                                                           array("bla" => "3 "),
+                                                           array("bla" => "4 "),
+                                                           array("bla" => "5 ")
+                                                           )));
     $this->assertEqual($res, "1 2 3 4 5 \n");
     
   }
 
   function testLambdaContext() {
-    $m = new Mustache();
+    $m = $this->m;
     $res = $m->render("{{foo}}",
                       array("a" => 42,
                             "foo" => function () { $ctx = Mustache\Context::GetContext(); return $ctx['a']; }));
